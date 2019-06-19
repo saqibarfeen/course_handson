bbb318f2decd5c0fa19aec731f9f9f5378e1b662
saqibarfeen <k180960@nu.edu.pk>
2019-06-19 12:29:28 +0500

    a search autocomplete

--- app.py
+++ app.py
@@ -3,7 +3,23 @@ from flask import Flask,render_template,request
 
 
 app= Flask(__name__)
+WORDS=[]
+with open('large','r') as file:
+    for line in file:
+        WORDS.append(line.rstrip())
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    x=request.args.get("x")
+    return render_template("index.html",x=x)
+
+@app.route('/search')
+def search():
+    q= request.args.get('q')
+    words=[]
+    for word in WORDS:
+        if word.startswith(q):
+            words.append(word)
+    return render_template("search.html",q=words)
+    #a=['aspect', 'alphas', 'alfanso', 'bravo','charlie']
